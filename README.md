9493b565de6f83a6e87dce06102e4bb91f203f55
octanium91 <octanium91@gmail.com>
2019-11-24 02:19:01 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# NFS_Heat_CPU_Load_FIX
- Need for Speed Heat, CPU 90-100% load fix (script, windows)
+# NFS_Heat_CPU_Load_FIX<br/>
+ Need for Speed Heat, CPU 90-100% load fix (script, windows)<br/>
+ <br/>
+ ## Instruction<br/>
+ <br/>
+ - copy **nfs_hits_cpu90_fix.cmd** in *Need for Speed Heat* main folder<br/>
+ - run **nfs_hits_cpu90_fix.cmd** *(script create user.cfg fot your CPU)*<br/>
+ - start game, be heppy :)<br/>
+ <br/>
+ *that’s about to work out...*<br/>
+ <img src="./img.jpg" width="579"><br/>
+ <br/>
---
+++ nfs_hits_cpu90_fix.cmd
@@ -0,0 +1,58 @@
+@echo off
+title = Fix 90%% load for NEED FOR SPEED HEAT. by Octanium
+set "CPU_Cores=0"
+set "CPU_Threads=0"
+set "UserCFGFileCDDir=%~dp0"
+set "UserCFGFileName=user.cfg"
+set "UserCFGFile=%UserCFGFileCDDir%%UserCFGFileName%"
+for /f "tokens=2 delims= " %%a in ('"wmic CPU Get DeviceID,NumberOfCores,NumberOfLogicalProcessors"') do set "CPU_Cores=%%a"
+for /f "tokens=3 delims= " %%a in ('"wmic CPU Get DeviceID,NumberOfCores,NumberOfLogicalProcessors"') do set "CPU_Threads=%%a"
+if %CPU_Cores% == 0 goto oops_a
+echo.
+echo    Fix 90%% CPU load!
+echo    For game NEED FOR SPEED HEAT
+echo    by Octanium
+echo.    
+echo  ==== Your CPU ====
+echo   CPU cores  : %CPU_Cores%
+echo   CPU threads: %CPU_Threads%
+echo  ==================
+REM Create user.cfg
+if exist "%UserCFGFile%" (
+    if exist "%UserCFGFileCDDir%%UserCFGFileName%.bak" (
+        del "%UserCFGFile%" /q /f
+    ) else (
+        rename "%UserCFGFileCDDir%%UserCFGFileName%" "%UserCFGFileName%.bak"
+    )   
+)
+echo Thread.ProcessorCount %CPU_Cores% >> "%UserCFGFile%"
+echo Thread.MaxProcessorCount %CPU_Cores% >> "%UserCFGFile%"
+echo Thread.MinFreeProcessorCount 0 >> "%UserCFGFile%"
+echo Thread.JobThreadPriority 0 >> "%UserCFGFile%"
+echo GstRender.Thread.MaxProcessorCount %CPU_Threads% >> "%UserCFGFile%"
+if exist "%UserCFGFile%" (
+    echo.
+    echo  =============================
+    echo   File user.cfg created!
+    echo  =============================
+    echo.
+    ) else (
+    echo.
+    echo  =============================
+    echo   File user.cfg NOT created!
+    echo  =============================
+    echo.
+)
+pause
+exit
+
+:oops_a
+cls
+echo.
+echo  =============================================
+echo    Oooops something went wrong :( (code: a)
+echo  =============================================
+echo.
+echo.
+pause
+exit
